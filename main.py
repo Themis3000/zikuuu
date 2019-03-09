ab589980405893beeacb1611e0353bce9ae37dad
Themi <Tcm4760@gmail.com>
2019-03-09 13:06:37 -0600

    -now command prefix setting in config.json does its thing. very cool.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 # todo:Themi figure out a better help format method, send help to users dm.
 
-client = commands.Bot(command_prefix='+', status=discord.Status(check_current("status_booting")), activity=discord.Game(name=check_current("game_booting")))
+client = commands.Bot(command_prefix=check_current("prefix"), status=discord.Status(check_current("status_booting")), activity=discord.Game(name=check_current("game_booting")))
 
 
 for file in os.listdir("cogs"):
